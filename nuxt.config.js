003f9daad5ff5006374f81e30f7f5b8d90ae7bcd
coolucifer <1045606768@qq.com>
2019-07-01 15:49:38 +0800

    解决跨域问题

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,10 @@ export default {
   /*
   ** Global CSS
   */
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: [
+    'assets/css/common.css',
+    'element-ui/lib/theme-chalk/index.css',
+  ],
   /*
   ** Plugins to load before mounting the App
   */
@@ -53,7 +56,9 @@ export default {
   },
   proxy: {
     '/api': {
-      target: 'http://doco.dev',
+      target: 'https://doco.dev',
+      // 开启跨域
+      changeOrigin: true,
     },
   },
   /*
