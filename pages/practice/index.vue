b2fea382d07597000b439e3b6c0756873b2eca8f
victor <1045606768@qq.com>
2019-07-02 21:46:19 +0800

    添加练习展示页面

---
+++ pages/practice/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="practice-index">
+    <article-card
+      v-for="item in list"
+      :key="item.id"
+      :item="item"
+      @click.native="cardClick(item.id)"
+    >
+    </article-card>
+  </div>
+</template>
+
+<script>
+import ArticleCard from '@/components/ArticleCard';
+
+export default {
+  layout: 'blog',
+  components: {
+    ArticleCard,
+  },
+  data() {
+    return {
+      // 练习列表
+      list: [
+        {
+          author: 'admin',
+          createTime: '2019-03-31',
+          id: 'tree',
+          title: 'Canvas绘制树形图',
+          updateTime: '2019-03-31',
+        },
+        {
+          author: 'admin',
+          createTime: '2019-02-24',
+          id: 'repeater',
+          title: '人类的本质',
+          updateTime: '2019-02-24',
+        },
+        {
+          author: 'admin',
+          createTime: '2018-01-28',
+          id: 'colorfulballs',
+          title: '绚丽小球',
+          updateTime: '2019-01-28',
+        },
+      ],
+    };
+  },
+  methods: {
+    cardClick(id) {
+      this.$router.push(`/practice/${id}`);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
