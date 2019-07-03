b1d1f24dce9f03b37db96c974c80935afcd0a1fd
coolucifer <1045606768@qq.com>
2019-07-03 17:07:06 +0800

    添加about页

---
+++ pages/about/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about-me">
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blog',
+  name: 'AboutMe',
+  components: {},
+  data() {
+    return {
+    };
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.about-me {
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  min-height: calc(100vh - 100px);
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+}
+</style>
