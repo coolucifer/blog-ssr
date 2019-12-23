49ce7436b492e81b0c35e83b0b48de3ab6956f5d
victor <1045606768@qq.com>
2019-12-23 20:40:41 +0800

    add resume

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about-me">
+    <iframe src="https://www.doco.dev/upload/resume.pdf" frameborder="0"></iframe>
   </div>
 </template>
 
@@ -28,5 +29,9 @@ export default {
   background-color: #fff;
   border-radius: 4px;
   box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+  iframe {
+    width: 100%;
+    height: calc(100vh - 130px);
+  }
 }
 </style>
